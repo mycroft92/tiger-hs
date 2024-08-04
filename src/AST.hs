b8e1845db1ff9e0963d218a718d024e7669b725b
Madhukar yerraguntla <mycroft@192.168.1.2>
2024-08-04 16:33:28 +0530

    Parser almost closing

--- src/AST.hs
+++ src/AST.hs
@@ -2,71 +2,27 @@ module AST where
     data Pos = Pos { line:: Int, col:: Int} deriving (Eq)
     data Range = Range {start:: Pos, stop:: Pos} deriving (Eq)
 
-    instance Show Pos where
-        show (Pos l c) = "("++show l++","++show c ++")"
-    
-    instance Show Range where
-        show (Range s e) = show s ++ "-" ++ show e 
- 
     data Var = SimpleVar String Range | FieldVar Var String Range | SubscriptVar Var Exp Range deriving (Show)
-
-    class Rangers c where
-        getRange :: c -> Range
-        getEnd   :: c -> Pos
-        getEnd c = stop $ getRange c
-        getStart :: c -> Pos
-        getStart c = start $ getRange c
-        {-# MINIMAL getRange #-}
-    
-    instance Rangers Var where
-        getRange (SimpleVar _ r)  = r
-        getRange (FieldVar _ _ r) = r
-        getRange (SubscriptVar _ _ r) = r 
 
     data Binop = Plus | Minus | Times | Divide | Eq | Neq | Lt | Le  | Gt | Ge | LAnd | LOr deriving (Show)
     data Field = Field {name:: String, value:: Exp, range :: Range} deriving (Show)
 
-    instance Rangers Field where
-        getRange (Field _ _ r) =r
+    -- data RecField = DecField String Bool String Range deriving (Show)
+    -- instance Rangers DecField where
+    --     getRange (DecField _ _ _ r) =r
 
-    data DecField = DecField String Bool Exp Range deriving (Show)
+    data FunDec = FunDec {fname:: String, params:: [RecField], result :: Maybe String, body:: Exp, frange :: Range} deriving (Show)
+    data TypeD  = TypeD String Typ Range deriving (Show)
+    data Typ    = NameTy String Range | RecordTy [RecField] Range | ArrayTy String Range deriving (Show)
 
-    instance Rangers DecField where
-        getRange (DecField _ _ _ r) =r
-
-    data FunDec = FunDec {fname:: String, params:: [DecField], result :: Maybe String, body:: Exp, frange :: Range} deriving (Show)
-
-    instance Rangers FunDec where
-        getRange (FunDec _ _ _ _ r) =r
-
-    data TypeD = TypeD String Typ Range deriving (Show)
-
-    instance Rangers TypeD where
-        getRange (TypeD _ _ r) =r
-
-    data Typ      = NameTy String Range | RecordTy [RecField] Range | ArrayTy String Range deriving (Show)
-
-    instance Rangers Typ where
-        getRange (NameTy _ r)   = r
-        getRange (RecordTy _ r) = r
-        getRange (ArrayTy _ r)  = r
     -- name, escaping, type, range
     data RecField = RecField String Bool String Range  deriving (Show)
-
-    instance Rangers RecField where
-        getRange (RecField _ _ _ r) = r
 
     data Dec = 
         FunctionDec [FunDec] Range
         -- name, escaping, Maybe type, init, range
         | VarDec String Bool (Maybe String) Exp Range
         | TypeDec [TypeD] Range deriving (Show)
-    
-    instance Rangers Dec where
-        getRange (FunctionDec _ r)  = r
-        getRange (VarDec _ _ _ _ r) = r
-        getRange (TypeDec _ r)      = r
-
     
     data Exp = 
         VarExp Var 
@@ -80,7 +36,7 @@ module AST where
         | RecordExp String [Field] Range
         | SeqExp [Exp] Range
         | AssignExp Var Exp Range
-        | IfExp Exp Exp Exp Range
+        | IfExp Exp Exp (Maybe Exp) Range
         | WhileExp  Exp Exp Range
         -- var name, escaping, lo-hi, body, range
         | ForExp String Bool Exp Exp Exp Range
@@ -88,6 +44,47 @@ module AST where
         | LetExp [Dec] Exp Range
         -- type, size, init, range
         | ArrayExp String Exp Exp Range deriving (Show)
+
+    instance Show Pos where
+        show (Pos l c) = "("++show l++","++show c ++")"
+    
+    instance Show Range where
+        show (Range s e) = show s ++ "-" ++ show e 
+
+    class Rangers c where
+        getRange :: c -> Range
+        getEnd   :: c -> Pos
+        getEnd c = stop $ getRange c
+        getStart :: c -> Pos
+        getStart c = start $ getRange c
+        {-# MINIMAL getRange #-}
+    
+    instance Rangers Field where
+        getRange (Field _ _ r) =r
+
+    instance Rangers Var where
+        getRange (SimpleVar _ r)  = r
+        getRange (FieldVar _ _ r) = r
+        getRange (SubscriptVar _ _ r) = r 
+
+    instance Rangers FunDec where
+        getRange (FunDec _ _ _ _ r) =r
+
+    instance Rangers Typ where
+        getRange (NameTy _ r)   = r
+        getRange (RecordTy _ r) = r
+        getRange (ArrayTy _ r)  = r
+
+    instance Rangers TypeD where
+        getRange (TypeD _ _ r) =r
+
+    instance Rangers RecField where
+        getRange (RecField _ _ _ r) = r
+    
+    instance Rangers Dec where
+        getRange (FunctionDec _ r)  = r
+        getRange (VarDec _ _ _ _ r) = r
+        getRange (TypeDec _ r)      = r
 
     instance Rangers Exp where
         getRange (VarExp v) = getRange v
@@ -106,6 +103,3 @@ module AST where
         getRange (BreakExp r)         = r
         getRange (LetExp _ _ r)       = r
         getRange (ArrayExp _ _ _ r)   = r
-
-    
-
--- src/Parser.hs
+++ src/Parser.hs
@@ -21,9 +21,10 @@ module Parser where
     import Data.Void (Void)
     import Data.Char (isAlphaNum, isAlpha, isPrint, toUpper, chr, isDigit)
     import Data.Text (unpack)
+    import Text.Megaparsec.Debug (dbg)
     -- import Control.Monad (void)
     import Data.Functor ( void, ($>) )
-
+    
 
     -- data ParserState = ParserState {
     --     errors   :: [Errors],
@@ -39,7 +40,7 @@ module Parser where
     --   (Left err, st) -> Left $ ParserError (Text.Megaparsec.Error.errorBundlePretty err) : errors st
     --   (Right e, _)  -> Right e
     parse :: FilePath -> T.Text -> Either Errors Exp
-    parse fname src = case runParser strparse fname src of
+    parse fname src = case runParser (expr <* eof) fname src of
       Left err -> Left $ ParserError (Text.Megaparsec.Error.errorBundlePretty err)
       Right e  -> Right e
 
@@ -59,18 +60,23 @@ module Parser where
     symbol :: T.Text -> Parser T.Text
     symbol = L.symbol space
 
+    symbol' :: T.Text -> Parser String
+    symbol' x = T.unpack <$> L.symbol space x
+
     lexeme :: Parser a -> Parser a
     lexeme = L.lexeme space
 
     brackets :: Parser a -> Parser a
-    brackets = between (symbol "[") (symbol "]")
+    brackets x = space *> between (symbol "[") (symbol "]") x
+
+    curly :: Parser a -> Parser a
+    curly x = space *> between (symbol "{") (symbol "}") x
 
     parens :: Parser a -> Parser a
-    parens = between (symbol "(") (symbol ")")
+    parens x = space *> between (symbol "(") (symbol ")") x
 
     integer :: Parser Int
     integer = lexeme L.decimal <?> "integer"
-
 
     annotate :: Parser (Range -> a) -> Parser a
     annotate m = do
@@ -148,10 +154,20 @@ module Parser where
       start <- getSrcPos
       s     <- satisfy isAlpha
       name  <- Data.Text.unpack <$> takeWhileP (Just "identifier") (\x -> isAlphaNum x || x == '_')
-      if name `elem` keywords then
+      if (s:name) `elem` keywords then
         fail $ "keyword found: "++show name++"@"++show start++", when identifier expected."
       else
         return (s:name)
+
+    keyword :: String -> Parser String
+    keyword t = space *> (do
+      start <- getSrcPos
+      s     <- satisfy isAlpha
+      name  <- Data.Text.unpack <$> takeWhileP (Just "keyword") (\x -> isAlphaNum x || x == '_')
+      if (s:name) == t then
+        return (s:name)
+      else
+        fail $ "identifier found: "++show name++"@"++show start++", when keyword expected.") <* space
 
     operators :: [[Operator Parser Exp]]
     operators = [[minus],
@@ -175,29 +191,31 @@ module Parser where
 
     binary :: (Parser (Exp -> Exp -> Exp) -> Operator Parser Exp) -> T.Text -> Binop -> Operator Parser Exp
     binary typ name f = typ  $ do
+      void space
       operator name
       return $ \e1 e2 ->
         let start = getStart e1 in
           let end = getEnd e2 in
             BinopExp e1 f e2 (Range start end)
 
-    assignParser :: Parser Exp
-    assignParser = undefined
-    
+    -- assignParser :: Parser Exp
+    -- assignParser = undefined
+
     lvalue :: Parser Var
     lvalue =  do
+      void space
       start <- getSrcPos
       id    <- lexeme identifier
       end   <- getSrcPos
       lvalue' (SimpleVar id (Range start end))
       where
         lvalue' :: Var -> Parser Var
-        lvalue' v = do 
+        lvalue' v = do
           lookAhead (optional anySingle) >>= \case
             Just '.' -> fieldId v
             Just '[' -> subscriptId v
             _        -> return v
-        
+
         fieldId :: Var -> Parser Var
         fieldId v = do
           start <- getSrcPos
@@ -213,15 +231,176 @@ module Parser where
           end   <- getSrcPos
           lvalue' $ SubscriptVar v exp (Range start end)
 
+    ite :: Parser Exp
+    ite = annotate $ do
+      void (keyword "if" <?> "if")
+      cond <- expr <?> "condition exp"
+      void (keyword "then")
+      texp <- expr <?> "true exp"
+      void (keyword "else")
+      fexp <- expr <?> "false exp"
+      return $ IfExp cond texp (Just fexp)
+
+    ite' :: Parser Exp
+    ite' = annotate $  do
+      void (keyword "if" <?> "if")
+      cond <- expr <?> "condition exp"
+      void (keyword "then")
+      texp <- expr <?> "true exp"
+      return $ IfExp cond texp Nothing
+    
+    ifexp :: Parser Exp
+    ifexp = try ite <|> ite'
+
+    whileexp :: Parser Exp
+    whileexp = undefined
+
+
     term :: Parser Exp
     term = try strparse
-      <|> annotate (IntExp <$> lexeme integer)
-      <|> annotate (symbol "nil" $> NilExp)
-      <|> assignParser
+      <|> try (annotate (IntExp <$> lexeme integer))
+      <|> try (annotate (keyword "nil" $> NilExp))
+      <|> try (annotate (SeqExp <$> lexeme (parens (sepBy expr (symbol ";")))))
+      -- record, callexp and lvalue all start with same identifier rule.
+      <|> try (annotate callexp)
+      <|> try (annotate recordexp)
+      <|> try (annotate arrayexp)
+      <|> try (annotate assignexp)
+      <|> try break
+      <|> try ifexp
+      <|>  (VarExp <$> lvalue)
+
+      where
+        callexp :: Parser (Range -> Exp)
+        callexp = do 
+          id   <- lexeme identifier <?> "Function call identifier"
+          exps <- lexeme $ parens (sepBy expr (symbol ","))
+          return $ CallExp id exps
+
+        recordexp :: Parser (Range -> Exp)
+        recordexp = do
+          tyid <- lexeme identifier <?> "Record type name"
+          vals <- curly (sepBy fieldexp (symbol ","))
+          return $ RecordExp tyid vals
+
+        fieldexp :: Parser Field
+        fieldexp = space *> annotate (do
+          id  <- lexeme identifier <?> "record field identifier" 
+          void (symbol "=" <?> "record assignment")
+          Field id <$> expr)
+        
+        arrayexp :: Parser (Range -> Exp)
+        arrayexp = do
+          id <- lexeme identifier <?> "Array identifier"
+          exp1 <- brackets expr <?> "Subscript exp"
+          void (keyword "of")
+          exp2 <- lexeme expr <?> "Value exp"
+          return $ ArrayExp id exp1 exp2
+
+        assignexp :: Parser (Range -> Exp)
+        assignexp = do
+          lval <- lexeme lvalue <?> "assign Lvalue"
+          void (symbol ":=")
+          AssignExp lval <$> lexeme expr
+
+
+        break :: Parser Exp
+        break = annotate $ void (keyword "break") $> BreakExp
+
+      -- <|> assignParser
 
     expr :: Parser Exp
-    expr = makeExprParser term operators
+    expr = space *> makeExprParser term operators
 
+    -- id : type 
+    typeField :: Parser RecField
+    typeField = annotate $ do
+      _  <- space
+      id <- lexeme identifier <?> "param name"
+      void (symbol ":")
+      tyid <- lexeme identifier <?> "param type"
+      return $ RecField id False tyid
 
+    typeFields :: Parser [RecField]
+    typeFields = sepBy typeField (symbol ",")
 
+    tydecs :: Parser Dec
+    tydecs = space *> annotate (TypeDec <$> some tydec <?> "Type declarations")
+      where
+        tydec :: Parser TypeD
+        tydec = space *> annotate (parser <?> "type declaration")
+          where
+            parser = do
+              void $ lexeme "type"
+              tyid <- lexeme identifier
+              void $ symbol "="
+              lookAhead (optional $ lexeme identifier <|> keyword "array" <|> symbol' "{") >>= \case
+                Just "array" -> arrayType tyid
+                Just "{"     -> fieldDecl tyid
+                Just  _      -> do
+                  ty <- annotate (NameTy <$> lexeme identifier)
+                  return $ TypeD tyid ty
+                Nothing      -> fail $ show tyid ++ ":: Not a valid type declaration!"
 
+            arrayType :: String -> Parser (Range -> TypeD)
+            arrayType x = do
+              start <- getSrcPos
+              void (keyword "array")
+              void (keyword "of")
+              arrty <- lexeme identifier <?> "array type identifier"
+              TypeD x . ArrayTy arrty . Range start <$> getSrcPos
+
+            fieldDecl :: String -> Parser (Range -> TypeD)
+            fieldDecl x = do
+              start <- getSrcPos
+              xs    <- between (symbol "{") (symbol "}") typeFields
+              TypeD x . RecordTy xs . Range start <$> getSrcPos
+
+    vardec :: Parser Dec
+    vardec = space *> annotate (do
+      void (keyword "var")
+      varid <- lexeme identifier
+      lookAhead (optional $ try (lexeme identifier)) >>= \case
+        Just tyid -> withType varid tyid
+        Nothing   -> withoutType varid)
+      where
+        withType varid tyid = do
+          void (symbol ":=")
+          exp <- lexeme expr
+          return $ VarDec varid False (Just tyid) exp
+
+        withoutType varid = do
+          void (symbol ":=")
+          exp <- lexeme expr
+          return $ VarDec varid False Nothing exp
+    
+    fundecs :: Parser Dec
+    fundecs = annotate (FunctionDec <$> some fundec <?> "function declaration block")
+      where
+        fundec :: Parser FunDec
+        fundec = space *> annotate (do
+          void (keyword "function" <?> "function keyword")
+          id <- lexeme identifier <?> "function name"
+          params <- parens typeFields <?> "function arguments"
+          lookAhead (space *> optional anySingle) >>= \case
+            Just ':' -> do
+              void (space *> symbol ":")
+              retid <- lexeme identifier <?> "return type"
+              FunDec id params (Just retid) <$> parseExp
+            Just '=' -> FunDec id params Nothing <$> parseExp
+            _ -> fail "Expected '=' or ':' after function declaration."
+          )
+          where
+            parseExp :: Parser Exp
+            parseExp = do
+              void (space *> symbol "=")
+              expr
+
+    dec  :: Parser Dec
+    dec = tydecs <|> vardec <|> fundecs
+
+    exprtest1 :: Either (ParseErrorBundle T.Text Void) Exp
+    exprtest1 = Text.Megaparsec.parse expr "" " a+b = c-d*5"
+
+    exprtest2 :: Either (ParseErrorBundle T.Text Void) Exp
+    exprtest2 = Text.Megaparsec.parse expr "" "      (    a  + b = b-c*5 ;break    )"
