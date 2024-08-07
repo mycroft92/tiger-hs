43f1992f54ffa176be4413b9a799c22973efbce1
Madhukar Yerraguntla <rathnakar.y@nxp.com>
2024-08-07 17:54:54 +0530

    Main handler

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,39 @@
 module Main (main) where
 
-import Lib
+import Lib (runFile)
+
+import System.Environment
+import System.IO
+import qualified System.Exit as Exit
+
+banner :: String
+banner = "\
+\################################################\n\
+\                 TIGER COMPILER            \n\
+\              Author: Madhukar Y.R            \n\
+\################################################"
 
 main :: IO ()
-main = someFunc
+main = do
+        putStrLn banner
+        args <- getArgs
+        progName <- getProgName
+        putStrLn  $ "Running: "++ progName
+
+        if null args
+            then do
+                putStrLn "Interpreter mode:"
+                -- hSetBuffering stdin NoBuffering
+                hSetBuffering stdout NoBuffering
+                -- runPrompt
+                Exit.exitSuccess
+            else
+                if not (null args) then do
+                    putStrLn $ "Running program "++ head args
+                    x <- runFile $ head args
+                    if x == 0 
+                        then Exit.exitSuccess 
+                        else Exit.exitFailure
+                else do
+                    putStrLn "Usage: tiger-hs <file.lox>"
+                    Exit.exitSuccess
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
-    ( someFunc
+    ( someFunc,
+    runFile
     ) where
+
+
+import Parser (parse)
+import Data.Text as T
+
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+runFile :: String -> IO Int
+runFile s = do
+    contents <- readFile s
+    case (parse s (T.pack contents)) of
+        Left err  -> print err >> return 1
+        Right exp -> print exp >> return 0
